fff08a06de0548dd600bf4330967d3212aaa88d2
punith369 <67690520+punith369@users.noreply.github.com>
2021-08-25 10:25:12 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome To The Band Name Generator")
+city = input("What's the name of your city")
+pet = input("what's the name of your pet")
+print("Yor Band Name could be " +city + " " +pet)
